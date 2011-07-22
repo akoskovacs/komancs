06971bae0b7e9e140152a0058965d366834dd81b
Ákos Kovács <akoskovacs@gmx.com>
2011-07-22 05:56:06 +0200

    File - New FileNotFoundException with new testing methods.

--- File.cxx
+++ File.cxx
@@ -5,28 +5,28 @@
 #include <unistd.h>
 #include <errno.h>
 
+#include <fstream>
 using namespace komacs;
 
-File::File(const std::string &fileName, std::ios::openmode m)
-    :m_fileName(fileNames), m_openMode(m)
-{ 
-    open();
+namespace {
+    struct stat getStat(const std::string &);
 }
+
+File::File(const std::string &fileName, std::ios::openmode m)
+    :m_fileName(fileName), m_openMode(m)
+{  open(); }
 
 File::File(const std::fstream &stream)
-{
-    m_fileName = stream.
-}
+    : m_fileName(stream.name())
+{ }
 
 File::~File()
-{
-    close();
-}
+{ close(); }
 
 void File::open()
 {
     m_file.open(m_fileName, m_openMode);
-    m_isFileOpened(true)
+    m_isFileOpened(true);
 
 }
 
@@ -48,24 +48,66 @@ File &File::setOpenMode(FileMode f)
     return *this;
 }
 
-static off_t File::size(const std::string &fileName) const
+off_t File::size()
 {
+    return size(m_fileName);
+}
+
+off_t File::size(const std::string &fileName) const
+{
+    return getStat(fileName).st_size;
+}
+
+bool File::isRegularFile(const std::string &fileName) const
+{
+    return (bool)S_ISREG(getStat(fileName).st_mode);
+}
+
+bool File::isDirectory(const std::string &fileName) const
+{
+    return (bool)S_ISDIR(getStat(fileName).st_mode);
+}
+
+bool File::isExist(const std::string &fileName) const
+{
+    return isCapable(fileName, Exist);
+}
+
+bool File::isCapable(const std::string &fileName, AccessMode m) const
+{
+    switch (access(fileName.c_str(), m)) {
+        case 0:
+            return true;
+            break;
+
+        case EACCES:
+            return false;
+            break;
+
+        case ENOENT:
+            throw errors::FileNotFoundException();
+            break;
+
+        default:
+            throw errors::SystemException("access()");
+            break;
+    }
+}
+
+struct stat getStat(const std::string &fileName)
+{
+    const char *fname = fileName.c_str();
     struct ::stat sbuf;
-    if (stat(fileName.c_str(), &sbuf) == -1) {
+    if (stat(fname, &sbuf) == -1) {
         switch (errno) {
             case ENOENT:
-                throw FileNotFoundException();
-                break;
-
-            case ENOTDIR:
-                throw NotDirectoryException();
+                throw errors::FileNotFoundException();
                 break;
 
             default:
-                throw SystemException("stat()");
+                throw errors::SystemException("stat()");
                 break;
         }
     }
-
-    return sbuf.st_size;
+    return sbuf;
 }
--- File.hxx
+++ File.hxx
@@ -1,13 +1,37 @@
 #ifndef FILE_HXX
 #define FILE_HXX
 
+#include "Exceptions.hxx"
+
 #include <string>
 #include <fstream>
 
+#include <unistd.h>
+
 namespace komancs {
+    namespace errors {
+        class komancs::File;
+        class FileNotFoundException : public Exception
+        {
+        public:
+            FileNotFoundException(const std::string &fname)
+                : m_fileName(fname), Exception("File " + fname + " not found!")
+            { }
+
+            FileNotFoundException(const komancs::File &f)
+                : m_fileName(f.fileName())
+                , Exception("File " + f.fileName() + " not found!")
+            { }
+
+        private:
+            std::string m_fileName;
+        };
+    }
+
     class File
     {
     public:
+        enum AccessMode { Read = R_OK, Write = W_OK, Execute = X_OK, Exist = F_OK }
         File(const std::string &fileName
             , std::ios::openmode m = std::ios::read);
         File(const std::fstream &);
@@ -26,6 +50,7 @@ namespace komancs {
         static bool isRegularFile(const std::string &) const;
         static bool isDirectory(const std::string &) const;
         static bool isExist(const std::string &) const;
+        static bool isCapable(const std::string &, AccessMode);
         static off_t size(const std::string &) const;
 
     private:
