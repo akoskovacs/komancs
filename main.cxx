24db4bb52e2df8c612328b9c65919e97af45f371
Ákos Kovács <akoskovacs@gmx.com>
2012-02-29 17:09:21 +0100

    main.cxx added

---
+++ main.cxx
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "locale.h"
+#include "TcpServer.hxx"
+#include "TcpConnection.hxx"
+#include "Ipv4Address.hxx"
+#include "Exceptions.hxx"
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    try {
+        komancs::Ipv4Address local(komancs::Any, 3000);
+        std::cout << "Starting Komancs Webserver...\n\nListening for connections at port 3000...\n";
+        komancs::TcpServer serv(&local);
+        komancs::TcpConnection *c = 0;
+        char buffer[800];
+        buffer[799] = '\0';
+        while (1) {
+            c = serv.accept();
+            c->receive(buffer, 199);
+            c->send("HTTP/1.1 200 OK\r\n\r\n<html><body><h1>Hello, world!</h1></body></html>\r\n\r\n");
+            delete c;
+            std::cout << "Received: " << buffer << std::endl;
+        }
+    } catch (komancs::error::Exception &e) {
+        std::cout << e.what() << std::endl;
+    }
+}
