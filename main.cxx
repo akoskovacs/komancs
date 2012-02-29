03f20afad552a6a4d9bc6da161f949e73bb42148
Ákos Kovács <akoskovacs@gmx.com>
2012-02-29 17:46:52 +0100

    Better welcome screen

--- main.cxx
+++ main.cxx
@@ -10,7 +10,7 @@ int main()
     setlocale(LC_ALL, "");
     try {
         komancs::Ipv4Address local(komancs::Any, 3000);
-        std::cout << "Starting Komancs Webserver...\n\nListening for connections at port 3000...\n";
+        std::cout << "Starting Komancs Webserver...\n\nListening for connections at port 3000...\n\n";
         komancs::TcpServer serv(&local);
         komancs::TcpConnection *c = 0;
         char buffer[800];
@@ -18,7 +18,9 @@ int main()
         while (1) {
             c = serv.accept();
             c->receive(buffer, 199);
-            c->send("HTTP/1.1 200 OK\r\n\r\n<html><body><h1>Hello, world!</h1></body></html>\r\n\r\n");
+            c->send("HTTP/1.1 200 OK\r\n\r\n<html><head><title>It works!</title></head><body><h1>Hello, world!</h1>"
+                    "<hr><i>Powered by Komancs Webserver</i><br><i>Copyleft (C) Akos Kovacs"
+                    "</i></body></html>\r\n\r\n");
             delete c;
             std::cout << "Received: " << buffer << std::endl;
         }
